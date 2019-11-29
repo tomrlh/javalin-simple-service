6502cca3958ed38bc7ef4e4e7eb252f2fd3116dd
Thiago Medeiros <si-thiago@hotmail.com>
2019-11-28 21:56:02 -0300

    Migrating to Javalin

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import io.javalin.Javalin;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Javalin app = Javalin.create()
+        .start(getHerokuAssignedPort())
+        .get("/", ctx -> ctx.result("Hello Heroku"));
+  }
+
+  private static int getHerokuAssignedPort() {
+    String herokuPort = System.getenv("PORT");
+    if (herokuPort != null) {
+      return Integer.parseInt(herokuPort);
+    }
+    return 7000;
+  }
+
+}
