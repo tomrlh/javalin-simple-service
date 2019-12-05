c1a8f4834910ceb28fda7a1472863eebd630779d
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-04 21:41:23 -0300

    Allowing Origin localhost:8050

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Javalin app = Javalin.create()
-				.start(getHerokuAssignedPort()).get("/", ctx -> ctx.result("Hello Heroku")); 
+				.start(getHerokuAssignedPort()).get("/", ctx -> ctx.result("Hello! You have crossed CORS policy")); 
+				app.enableCorsForOrigin("http://localhost:8050");
 	}
 
 	private static int getHerokuAssignedPort() {
